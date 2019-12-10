e11e1ff7d8d9bf5093ef5815b1e6ff5195376a2a
TT527 <1019599978@qq.com>
2019-12-10 20:26:00 +0800

    init

---
+++ src/main/java/com/jxsoft/UserRoleSystem/UserRoleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jxsoft.UserRoleSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRoleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRoleSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jxsoft/UserRoleSystem/UserRoleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxsoft.UserRoleSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRoleSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

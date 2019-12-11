10a6f00d107bcae34e13e4118d44edb730dbe855
chlang <lang.cheng@jinxiangtec.com>
2019-12-11 08:44:08 +0800

    项目初始化

--- src/test/java/com/jinxiang/user_role_system/UserRoleSystemApplicationTests.java
+++ src/test/java/com/jinxiang/user_role_system/UserRoleSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jxsoft.UserRoleSystem;
+package com.jinxiang.user_role_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserRoleSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
